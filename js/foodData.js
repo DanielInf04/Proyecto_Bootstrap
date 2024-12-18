f6d7a4980a735822e83ea38f2889ca6434a5a4fa
DanielInf04 <danielinf0417@gmail.com>
2024-12-18 01:10:49 +0100

    solucionado error 2

--- js/foodData.js
+++ js/foodData.js
@@ -2,121 +2,121 @@ const foodData = [
     {
         name: "Choripán Fueguino",
         description: "Choripán con chorizo artesanal y chimichurri patagónico.",
-        image: "../img/food/choripan.jpg"
+        image: "../img/Comida/choripan.jpg"
     },
     { 
         name: "Trucha del Canal Beagle", 
         description: "Pescado fresco de las aguas cristalinas de la región.", 
-        image: "../img/food/trucha.jpg" 
+        image: "../img/Comida/trucha.jpg" 
     },
     { 
         name: "Chocolates artesanales", 
         description: "Chocolate de producción local, con un sabor único y suave.", 
-        image: "../img/food/chocolates.jpg" 
+        image: "../img/Comida/chocolates.jpg" 
     },
     { 
         name: "Empanadas de carne", 
         description: "Empanadas rellenas con carne de cordero y especias regionales.", 
-        image: "../img/food/empanadas.jpg" 
+        image: "../img/Comida/empanadas.jpg" 
     },
     { 
         name: "Repostería regional", 
         description: "Postres deliciosos que capturan los sabores únicos de la región.", 
-        image: "../img/food/reposteria.jpg" 
+        image: "../img/Comida/reposteria.jpg" 
     },
     { 
         name: "Almejas patagónicas", 
         description: "Almejas frescas recogidas del océano, cocinadas con manteca y ajo.", 
-        image: "../img/food/almejas.jpg" 
+        image: "../img/Comida/almejas.jpg" 
     },
     { 
         name: "Calafate con crema", 
         description: "Frutos de calafate servidos con crema casera, un postre típico.", 
-        image: "../img/food/calafate.jpg" 
+        image: "../img/Comida/calafate.jpg" 
     },
     { 
         name: "Merluza negra", 
         description: "Un pescado local que se sirve con salsa de mariscos y puré de papas.", 
-        image: "../img/food/merluza.jpg" 
+        image: "../img/Comida/merluza.jpg" 
     },
     { 
         name: "Chupe de mariscos", 
         description: "Plato tradicional con mariscos frescos y un toque cremoso.", 
-        image: "../img/food/chupe.jpg" 
+        image: "../img/Comida/chupe.jpg" 
     },
     { 
         name: "Tarta de frutos rojos", 
         description: "Deliciosa tarta con frutos rojos, muy popular en la región.", 
-        image: "../img/food/tarta.jpg" 
+        image: "../img/Comida/tarta.jpg" 
     },
     { 
         name: "Pechuga de pato con salsa de ciruelas", 
         description: "Exquisita pechuga de pato acompañada con una salsa dulce de ciruelas.", 
-        image: "../img/food/pato.jpg" 
+        image: "../img/Comida/pato.jpg" 
     },
     { 
         name: "Asado Fueguino", 
         description: "Carne asada al estilo Fueguino, un platillo que resalta los sabores locales.", 
-        image: "../img/food/asado.jpg" 
+        image: "../img/Comida/asado.jpg" 
     },
     { 
         name: "Sopa de camarones", 
         description: "Sopa de camarones frescos, ideal para los amantes del marisco.", 
-        image: "../img/food/sopa_camarones.jpg" 
+        image: "../img/Comida/sopa_camarones.jpg" 
     },
     { 
         name: "Tamal de cerdo", 
         description: "Tamal relleno de cerdo, una combinación deliciosa de sabores.", 
-        image: "../img/food/tamal.jpg" 
+        image: "../img/Comida/tamal.jpg" 
     },
     { 
         name: "Galletas de avena y miel", 
         description: "Galletas caseras con avena y miel de producción local.", 
-        image: "../img/food/galletas.jpg" 
+        image: "../img/Comida/galletas.jpg" 
     },
     { 
         name: "Torta de manzana", 
         description: "Torta casera con manzanas frescas, un toque suave de canela.", 
-        image: "../img/food/torta_manzana.jpg" 
+        image: "../img/Comida/torta_manzana.jpg" 
     },
     { 
         name: "Papas a la crema", 
         description: "Papas cocidas con una salsa cremosa de queso y cebollas.", 
-        image: "../img/food/papas_crema.jpg" 
+        image: "../img/Comida/papas_crema.jpg" 
     },
     { 
         name: "Lenguado a la parrilla", 
         description: "Pescado fresco del Canal Beagle, cocinado a la parrilla con un toque de hierbas locales.", 
-        image: "../img/food/lenguado_parrilla.jpg" 
+        image: "../img/Comida/lenguado_parrilla.jpg" 
     },
     { 
         name: "Matambre a la pizza", 
         description: "Carne de matambre de res cubierta con salsa de tomate, queso y jamón, cocida a la parrilla.", 
-        image: "../img/food/matambre_pizza.jpg" 
+        image: "../img/Comida/matambre_pizza.jpg" 
     },
     { 
         name: "Empanadas de cordero", 
         description: "Empanadas rellenas con carne de cordero, especias regionales y un toque de cebolla.", 
-        image: "../img/food/empanadas_cordero.jpg" 
+        image: "../img/Comida/empanadas_cordero.jpg" 
     },
     {
         name: "Centolla Fueguina",
         description: "Un plato emblemático hecho con centolla fresca capturada en las aguas del Canal Beagle.",
-        image: "../img/food/centolla.jpg"
+        image: "../img/Comida/centolla.jpg"
     },
     {
         name: "Locro Patagónico",
         description: "Un guiso tradicional con maíz, carne y especias típicas de la región.",
-        image: "../img/food/locro.jpeg"
+        image: "../img/Comida/locro.jpeg"
     },
     {
         name: "Rabas",
         description: "Anillos de calamar rebozados y fritos, una delicia marina muy consumida en la región.",
-        image: "../img/food/rabas.jpg"
+        image: "../img/Comida/rabas.jpg"
     },
     {
         name: "Pastel de Papa y Cordero",
         description: "Un pastel casero relleno con cordero patagónico, cubierto con puré de papas cremoso.",
-        image: "../img/food/pastel_papa_cordero.jpg"
+        image: "../img/Comida/pastel_papa_cordero.jpg"
     }
 ];
